acc2d26fcd13daaadce2bbe2ac0277ab86de341c
Ubuntu <ubuntu@ip-172-31-8-204.us-west-2.compute.internal>
2013-07-06 05:48:01 +0000

    Updated scripts

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-$ npm install cheerio
-$ npm install commander
+npm install cheerio
+npm install commander
 
-git clone https://github.com/dpyryesk/bitstarter.git
-
-cd bitstarter/
-
-chmod 700 grader.js
+git clone https://github.com/dpyryesk/bitstarter.git ~/bitstarter
