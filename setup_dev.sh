85e1c0aed59ac0a0bf6ed74211dff660da437fe9
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2013-07-06 01:32:12 -0400

    New setup files

---
+++ setup_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$ npm install cheerio
+$ npm install commander
+
+git clone https://github.com/dpyryesk/bitstarter.git
+
+cd bitstarter/
+
+chmod 700 grader.js
---
+++ setup_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/dpyryesk/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
